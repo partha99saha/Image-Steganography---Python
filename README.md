78d45a2c9abbe412429963f6d56eb0abda306cb2
partha99saha <psaha.bdn9@gmail.com>
2024-02-04 15:43:18 +0530

    Code upgrade

---
+++ README.md
@@ -0,0 +1,5 @@
+In image steganography, a message is embedded into an image by altering the values of some pixels, 
+which are chosen by an encryption algorithm. The recipient of the image must be aware of the same algorithm in order to know which pixels he or she must select to extract the message
+
+Install the required libraries: pip install pillow
+Run the code: python app.py
--- app.py
+++ app.py
@@ -1,38 +1,25 @@
-# Python program implementing Image Steganography
-
-# PIL module is used to extract
-# pixels of image and modify it
 from PIL import Image
 
-# Convert encoding data into 8-bit binary
-# form using ASCII value of characters
+# Convert encoding data into 8-bit binary form using ASCII value of characters
 def genData(data):
-
-		# list of binary codes
-		# of given data
+		# list of binary codes of given data
 		newd = []
-
 		for i in data:
 			newd.append(format(ord(i), '08b'))
 		return newd
 
-# Pixels are modified according to the
-# 8-bit binary data and finally returned
+# Pixels are modified according to the 8-bit binary data and finally returned
 def modPix(pix, data):
-
 	datalist = genData(data)
 	lendata = len(datalist)
 	imdata = iter(pix)
 
 	for i in range(lendata):
-
 		# Extracting 3 pixels at a time
-		pix = [value for value in imdata.__next__()[:3] +
-								imdata.__next__()[:3] +
-								imdata.__next__()[:3]]
+		pix = [value for value in imdata.
+			__next__()[:3] + imdata.__next__()[:3] + imdata.__next__()[:3]]
 
-		# Pixel value should be made
-		# odd for 1 and even for 0
+		# Pixel value should be made odd for 1 and even for 0
 		for j in range(0, 8):
 			if (datalist[i][j] == '0' and pix[j]% 2 != 0):
 				pix[j] -= 1
@@ -42,19 +29,15 @@ def modPix(pix, data):
 					pix[j] -= 1
 				else:
 					pix[j] += 1
-				# pix[j] -= 1
 
-		# Eighth pixel of every set tells
-		# whether to stop ot read further.
-		# 0 means keep reading; 1 means thec
-		# message is over.
+		# Eighth pixel of every set tells whether to stop ot read further.
+		# 0 means keep reading; 1 means thec message is over.
 		if (i == lendata - 1):
 			if (pix[-1] % 2 == 0):
 				if(pix[-1] != 0):
 					pix[-1] -= 1
 				else:
 					pix[-1] += 1
-
 		else:
 			if (pix[-1] % 2 != 0):
 				pix[-1] -= 1
@@ -64,12 +47,12 @@ def modPix(pix, data):
 		yield pix[3:6]
 		yield pix[6:9]
 
+# Encode the Image
 def encode_enc(newimg, data):
 	w = newimg.size[0]
 	(x, y) = (0, 0)
 
 	for pixel in modPix(newimg.getdata(), data):
-
 		# Putting modified pixels in the new image
 		newimg.putpixel((x, y), pixel)
 		if (x == w - 1):
@@ -97,7 +80,6 @@ def encode():
 def decode():
 	img = input("Enter image name(with extension) : ")
 	image = Image.open(img, 'r')
-
 	data = ''
 	imgdata = iter(image.getdata())
 
@@ -108,7 +90,6 @@ def decode():
 
 		# string of binary data
 		binstr = ''
-
 		for i in pixels[:8]:
 			if (i % 2 == 0):
 				binstr += '0'
@@ -123,16 +104,14 @@ def decode():
 def main():
 	a = int(input(":: Welcome to Steganography ::\n"
 						"1. Encode\n2. Decode\n"))
+						
 	if (a == 1):
 		encode()
-
 	elif (a == 2):
 		print("Decoded Word : " + decode())
 	else:
 		raise Exception("Enter correct input")
 
-# Driver Code
+# calling main
 if __name__ == '__main__' :
-
-	# Calling main function
 	main()
